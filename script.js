4e9f577c2233136a9690879e39d102bd83a2793f
Alejandro <alejandrojaviercardozo2002@hotmail.com>
2026-02-04 19:37:52 -0300

    Se agrega un archivo .js para agregar la funcionalidad de modo oscuro y claro

---
+++ script.js
@@ -0,0 +1,20 @@
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+const icon = themeToggle.querySelector('i');
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('light-mode');
+    
+    if (body.classList.contains('light-mode')) {
+        icon.classList.replace('fa-moon', 'fa-sun');
+        localStorage.setItem('theme', 'light');
+    } else {
+        icon.classList.replace('fa-sun', 'fa-moon');
+        localStorage.setItem('theme', 'dark');
+    }
+});
+
+if (localStorage.getItem('theme') === 'light') {
+    body.classList.add('light-mode');
+    icon.classList.replace('fa-moon', 'fa-sun');
+}
